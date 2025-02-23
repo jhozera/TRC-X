653af0a80171243f6f41efc22d38252934ce6ba9
jhozera <152558072+jhozera@users.noreply.github.com>
2025-02-23 15:17:07 -0300

    daqui pouco

---
+++ javascript/navegação.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.corpo-cartoes', { 
+        duration: 1000,  
+        origin: 'top', 
+        distance: '50px', 
+        delay: 200, 
+        easing: 'ease-in-out', 
+        reset: true 
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.corpo-pr', { 
+        duration: 1000,  
+        origin: 'top', 
+        distance: '50px', 
+        delay: 200, 
+        easing: 'ease-in-out', 
+        reset: true 
+    });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.acordos-c6', { 
+        duration: 1000,  
+        origin: 'top', 
+        distance: '50px', 
+        delay: 200, 
+        easing: 'ease-in-out', 
+        reset: true 
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.app', { 
+        duration: 1000,  
+        origin: 'top', 
+        distance: '50px', 
+        delay: 200, 
+        easing: 'ease-in-out', 
+        reset: true 
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.banner', { 
+        duration: 1000,  
+        origin: 'top', 
+        distance: '50px', 
+        delay: 200, 
+        easing: 'ease-in-out', 
+        reset: true 
+    });
+});
+
