778f78bc24c580ce68300ccd67e45eea7278cc15
jhozera <152558072+jhozera@users.noreply.github.com>
2025-02-24 23:46:24 -0300

    medias quase la

--- javascript/modo.js
+++ javascript/modo.js
@@ -1,4 +1,6 @@
 const toggleSwitch = document.querySelector(".switch input");
     toggleSwitch.addEventListener("change", () => {
         document.body.classList.toggle("light-mode");
-    });
+});
+
+
